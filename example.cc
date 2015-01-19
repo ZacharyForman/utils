711f811d81890724fb0fb33fb7f0eeeb4229ccf1
ZacharyForman <au.zachary.forman@gmail.com>
2015-01-19 21:21:36 +1100

    Example code to demonstrate current use of sprintf.

---
+++ example.cc
@@ -0,0 +1,13 @@
+#include "sprintf.h"
+#include <cstdio>
+#include <string>
+
+using utils::sprintf;
+
+int main()
+{
+  std::string hello_msg = "Hello world!";
+  int integer = 12345;
+  std::string msg = sprintf("%v: %v", hello_msg, integer);
+  printf("%s\n", msg.c_str());
+}
