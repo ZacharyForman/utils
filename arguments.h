ca3ebe8f4388b336a78a23f34313dbbd56d7ff77
ZacharyForman <au.zachary.forman@gmail.com>
2015-07-07 00:02:50 +0930

    Reworked.

--- arguments.h
+++ arguments.h
@@ -1,7 +1,7 @@
 #ifndef ARGUMENTS_H
 #define ARGUMENTS_H
 
-#include <cstdlib>
+#include <algorithm>
 #include <iostream>
 #include <map>
 #include <memory>
@@ -16,10 +16,10 @@ namespace utils {
 template<typename T>
 std::string arg_error_text(std::string flag_name, std::string argument)
 {
-  return "error: `" + argument "' is a bad argument for " + flag_name + ".";
+  return "error: `" + argument + "' is a bad argument for " + flag_name + ".";
 }
 
-namespace {
+namespace internal {
 
 /// Internal abstract base class for a command line argument.
 class Arg {
@@ -30,11 +30,11 @@ public:
   ///   required: If the argument is required by the program.
   Arg(std::string flag, std::string help_text, bool required = false);
 
-  /// Parses the value from arg. In case of error, terminates the program.
-  /// Modifies this Arg's stored variable.
-  /// Returns true if the argument pointer should be moved along.
-  ///   arg: A null terminated string to extract the value from.
-  virtual bool parse_value(const char *arg) = 0;
+  /// Parses the value from args. Modifies this Arg's stored variable.
+  /// Returns how many arguments were consumed, or -1 on error.
+  ///   args: The arguments to consume.
+  ///   arg: The starting argument to consider.
+  virtual int parse_value(const char **args, int arg) = 0;
 
   /// Returns a string containing the current value of this argument.
   virtual std::string value() const = 0;
@@ -47,76 +47,85 @@ public:
   bool ok;
 };
 
-Arg(std::string flag, std::string help_text, bool required)
-  : flag(flag), help_text(help_text), required(required) {}
+Arg::Arg(std::string flag, std::string help_text, bool required)
+  : flag(flag), help_text(help_text), ok(!required) {}
 
 /// Internal class that actually stores the value of an argument.
 /// Boolean variables are always considered to be singular, i.e.
 /// --flag will set a boolean variable to the opposite of its default.
 template<typename T>
-class TypedArg final : Arg {
+class TypedArg final : public Arg {
 public:
   /// Constructs a TypedArg.
+  ///   var: A reference to the variable the argument should be stored in.
   ///   flag: The command line flag given, e.g. -l or --height.
   ///   help_text: Text printed when the --help arg is received.
-  ///   val: A reference to the variable the argument should be stored in.
   ///   required: If the argument is required by the program.
-  TypedArg(std::string flag, std::string help_text,
-           T &val, bool required = false);
+  TypedArg(T &var, std::string flag, std::string help_text,
+           bool required = false);
 
-  bool parse_value(const char *arg) override;
+  int parse_value(const char **args, int arg) override;
   std::string value() const override;
 private:
   /// A reference to the variable the received argument should be stored in.
   T &var;
 };
 
-TypedArg(std::string flag, std::string help_text,
-         T &val, bool required = false)
-  : Arg(flag, help_text, required), val(val) {}
+template<typename T>
+TypedArg<T>::TypedArg(T &var, std::string flag, std::string help_text,
+                      bool required)
+  : Arg(flag, help_text, required), var(var) {}
 
 template<typename T>
-bool TypedArg<T>::parse_value(const char *arg)
+int TypedArg<T>::parse_value(const char **args, int arg)
 {
-  std::stringstream ss(arg);
-  if (!(ss >> val)) {
-    std::cerr << arg_error_text<T>(flag, arg) << std::endl;
-    exit(1);
+  if (arg < 0) {
+    return -1;
+  }
+  std::stringstream ss(args[arg]);
+  if (!(ss >> var)) {
+    std::cerr << arg_error_text<T>(flag, args[arg]) << std::endl;
+    return -1;
   }
   ok = true;
-  return true;
+  return 1;
 }
 
 template<>
-bool TypedArg<bool>::parse_value(const char *arg)
+int TypedArg<bool>::parse_value(const char **args, int arg)
 {
   var = !var;
   ok = true;
-  return false;
+  return 0;
 }
 
 template<typename T>
-std::string TypedArg<T>::value()
+std::string TypedArg<T>::value() const
 {
   std::stringstream ss;
-  ss << val;
+  ss << var;
   return ss.str();
 }
 
-} // namespace
+} // internal
 
 /// Class that contains a list of arguments.
 /// Add arguments to the list using ArgList::add_arg(), then parse
 /// the argument list given to your program using Arglist::parse_args().
 class ArgList {
 public:
+  /// Constructs an ArgList with the given help flag.
+  ///   help_flag: The help flag to use. Defaults to --help.
+  ArgList(std::string help_flag = "--help");
+
   /// Adds an argument to this ArgList.
+  /// Returns true if another argument with the given flag is not registered.
   ///   val: The variable to store the argument in.
   ///   flag: The expected flag.
-  ///   help_text: Information about the flag, printed when --help is received.
+  ///   help_text: Information about the flag, printed if help is requested.
   ///   required: If the argument is required for success of the program.
   template<typename T>
-  void add_arg(T& val, std::string flag, std::string help_text,
+  bool add_arg(T& val, std::string flag, std::string help_text,
                bool required = false);
 
   /// Parses arguments from the argument list provided.
@@ -126,29 +135,81 @@ public:
   ///   argv: The argument vector, where the zeroth argument is ignored.
   int parse_args(int argc, const char **argv);
 
-  /// Prints associated help information to stdout.
+  /// Prints associated help information to stderr.
   void help();
+
+  static const bool required = true;
+  static const bool optional = false;
 private:
   /// Contains the arguments and information about them.
   /// The key will always be the flag of the Arg referenced.
-  std::map<const std::string&, std::unique_ptr<Arg>> arguments;
+  std::map<std::string, std::unique_ptr<internal::Arg>> arguments;
+  /// Flag that must be given to receive help text, --help by default.
+  const std::string help_flag;
 };
 
-template<typename T>
-void ArgList::add_arg(T& val, std::string flag, std::string help_text,
-                      bool required = false)
-{
+ArgList::ArgList(std::string help_flag)
+    : help_flag(help_flag) {}
 
+template<typename T>
+bool ArgList::add_arg(T& val, std::string flag, std::string help_text,
+                      bool required)
+{
+  if (arguments.find(flag) != arguments.end()) {
+    return false;
+  }
+  auto arg
+      = std::make_unique<internal::TypedArg<T>>(val, flag, help_text, required);
+  arguments[flag] = std::move(arg);
+  return true;
 }
 
 int ArgList::parse_args(int argc, const char **argv)
 {
+  for (int i = 1; i < argc; i++) {
+    if (help_flag == argv[i]) {
+      help();
+      exit(0);
+    }
 
+    if (arguments.find(argv[i]) == arguments.end()) {
+      std::cerr << "Bad argument " << argv[i] << std::endl;
+      std::cerr << argv[0] << " --help for assistance." << std::endl;
+      exit(1);
+    }
+
+    int res = arguments[argv[i]]->parse_value(argv, (i+1>=argc ? -1 : i+1));
+    if (res == -1 && i+1 == argc) {
+      std::cerr << "Not enough arguments for " << argv[i] << std::endl;
+      std::cerr << argv[0] << " --help for assistance." << std::endl;
+      exit(1);
+    } else if (res == -1) {
+      std::cerr << argv[0] << " --help for assistance." << std::endl;
+      exit(1);
+    }
+    i += res;
+  }
+
+  for (auto &arg : arguments) {
+    if (!arg.second->ok) {
+      std::cerr << arg.first << " is required." << std::endl;
+      std::cerr << argv[0] << " --help for assistance." << std::endl;
+      exit(1);
+    }
+  }
 }
 
 void ArgList::help()
 {
-
+  for (auto& arg : arguments) {
+    std::stringstream ss;
+    ss << "  " << arg.second->flag;
+    ss << std::string(std::max(1u, 20-ss.str().length()), ' ');
+    ss << "= " << arg.second->value();
+    ss << std::string(std::max(1u, 30-ss.str().length()), ' ');
+    ss << arg.second->help_text << std::endl;
+    std::cerr << ss.str();
+  }
 }
 
 } // utils
--- examples/arguments.cc
+++ examples/arguments.cc
@@ -1,4 +1,69 @@
-int main(int argc, char **argv)
-{
+#include "../arguments.h"
 
+#include <string>
+
+int main(int argc, const char **argv)
+{
+  // path to images.
+  std::string images_path = "images/";
+  // base dictionary path.
+  std::string dict_base_path = "dict/";
+  // Controls number of patches in dictionary.
+  int dict_size = 1024;
+  // Controls sparsity of coding.
+  float lambda = 0.15;
+  // Size of an image patch.
+  int patch_size = 3;
+  // Number of patches to sample.
+  int samples = 100000;
+  // Upscaling factor.
+  int upscale_factor = 3;
+  // If we train this run.
+  bool train = false;
+  // If we test this run.
+  bool test= false;
+  // Iterations to run the dict through
+  int iterations = 1000;
+  // Size of each training batch
+  int batch_size = 50;
+  // Extension of images to use
+  std::string extension = ".jpg";
+  // Variance threshold
+  float threshold = 0.1;
+  // Overlap in resolved images;
+  int overlap = 2;
+
+  utils::ArgList args;
+
+  args.add_arg(images_path, "--image-path",
+               "Path to the folder the images are in",
+               utils::ArgList::required);
+  args.add_arg(dict_base_path, "--dict-path",
+               "Path to the folder the dictionaries are in");
+  args.add_arg(dict_size, "--dict-size",
+               "The size of the dictionary used");
+  args.add_arg(lambda, "--lambda",
+               "Controls the sparsity of coding");
+  args.add_arg(patch_size, "--patch-size",
+               "Size of an image patch");
+  args.add_arg(samples, "--samples",
+               "Number of samples to train on");
+  args.add_arg(upscale_factor, "--upscale-factor",
+               "Upscaling factor");
+  args.add_arg(train, "--train",
+               "Train a coupled dictionary");
+  args.add_arg(test, "--test",
+               "Test the dictionary on the images given");
+  args.add_arg(iterations, "--iterations",
+               "Number of batches to run the dictionary through");
+  args.add_arg(batch_size, "--batch_size",
+               "Size of each training batch.");
+  args.add_arg(extension, "--ext",
+               "Extension of images to use");
+  args.add_arg(threshold, "--threshold",
+               "Threshold of variance to keep patches");
+  args.add_arg(overlap, "--overlap",
+               "Overlap of adjacent image patches");
+
+  args.parse_args(argc, argv);
 }
